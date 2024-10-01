59f38223ca8f10777af3f108f593bd449d77eb3c
Winoliya <win.sin.rt23@dypatil.edu>
2024-10-02 02:12:08 +0530

    Delete Products from Cart

--- data/cart.js
+++ data/cart.js
@@ -30,3 +30,17 @@ export function addToCart(productId)
       });
   }
 }
+
+export function removeFromCart(productId)
+{
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if(cartItem.productId !== productId)
+    {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+}
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formattingCurrency } from './utils/money.js';
 
@@ -40,7 +40,7 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -95,7 +95,20 @@ cart.forEach((cartItem) => {
 `
   });
 
+  //console.log(cartSumHTML);
   document.querySelector('.js-order-summary').innerHTML=cartSumHTML;
 
-console.log(cartSumHTML);
+  document.querySelectorAll('.js-delete-link').forEach((link) => {
+    link.addEventListener('click', () =>{
+      const productId = link.dataset.productId;
+
+      removeFromCart(productId);
+      //console.log(cart);
+    });
+
+  });
+
+
+
+
   
